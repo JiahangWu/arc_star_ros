476d94d5a6fb207b7e20d6057f6a8466e62235a4
Jiahang Wu <wujiahang.ai@gmail.com>
2023-07-17 10:30:13 +0800

    first commit

---
+++ arc_star/build/CMakeFiles/arc_star_app_file.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arc_star_app_file.dir/src/arc_star_app_file.cc.o"
+  "CMakeFiles/arc_star_app_file.dir/src/arc_star_app_file.cc.o.d"
+  "CMakeFiles/arc_star_app_file.dir/src/arc_star_detector.cc.o"
+  "CMakeFiles/arc_star_app_file.dir/src/arc_star_detector.cc.o.d"
+  "arc_star_app_file"
+  "arc_star_app_file.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arc_star_app_file.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ arc_star/build/CMakeFiles/arc_star_app_file.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for arc_star_app_file.
+# This may be replaced when dependencies are built.
